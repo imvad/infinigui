67499dd22ec16342fe31bcd215ec72ebc10c4a8b
Vadim All <vadik.all@gmail.com>
2021-12-22 18:47:42 +0200

    wip

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,157 +1,3 @@
-// import React, { ReactNode } from 'react'
-
-// import Table from '@mui/material/Table';
-// import TableBody from '@mui/material/TableBody';
-// import TableCell from '@mui/material/TableCell';
-// import TableContainer from '@mui/material/TableContainer';
-// import TableHead from '@mui/material/TableHead';
-// import TableRow from '@mui/material/TableRow';
-// import Paper from '@mui/material/Paper';
-
-
-// import Box from '@mui/material/Box';
-// import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-// import Button from '@mui/material/Button';
-// import List from '@mui/material/List';
-// import Divider from '@mui/material/Divider';
-// import ListItem from '@mui/material/ListItem';
-// import ListItemIcon from '@mui/material/ListItemIcon';
-// import ListItemText from '@mui/material/ListItemText';
-// import InboxIcon from '@mui/icons-material/MoveToInbox';
-// import MailIcon from '@mui/icons-material/Mail';
-
-
-// import customTheme from '../../lib/theme';
-// import './Table.css'
-
-// function createData(
-//   name: string,
-//   calories: number,
-//   fat: number,
-//   carbs: number,
-//   protein: number,
-// ) {
-//   return { name, calories, fat, carbs, protein };
-// }
-
-// const rows = [
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9),
-// ];
-
-
-// function ITable() {
-//   return (
-//     <TableContainer component={Paper}>
-//       <SwipeableTemporaryDrawer />
-//       <Table sx={{ minWidth: 650 }} aria-label="simple table">
-//         <TableHead>
-//           <TableRow>
-//             <TableCell>Dessert (100g serving)</TableCell>
-//             <TableCell align="right">Calories</TableCell>
-//             <TableCell align="right">Fat&nbsp;(g)</TableCell>
-//             <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-//             <TableCell align="right">Protein&nbsp;(g)</TableCell>
-//           </TableRow>
-//         </TableHead>
-//         <TableBody>
-//           {rows.map((row) => (
-//             <TableRow
-//               key={row.name}
-//               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-//             >
-//               <TableCell component="th" scope="row">
-//                 {row.name}
-//               </TableCell>
-//               <TableCell align="right">{row.calories}</TableCell>
-//               <TableCell align="right">{row.fat}</TableCell>
-//               <TableCell align="right">{row.carbs}</TableCell>
-//               <TableCell align="right">{row.protein}</TableCell>
-//             </TableRow>
-//           ))}
-//         </TableBody>
-//       </Table>
-//     </TableContainer>
-//   );
-// }
-
-// function SwipeableTemporaryDrawer() {
-//   const [state, setState] = React.useState({
-//     top: false,
-//     left: false,
-//     bottom: false,
-//     right: false,
-//   });
-
-//   type Anchor = 'top' | 'left' | 'bottom' | 'right';
-
-//   const toggleDrawer =
-//     (anchor: Anchor, open: boolean) =>
-//       (event: React.KeyboardEvent | React.MouseEvent) => {
-//         if (
-//           event &&
-//           event.type === 'keydown' &&
-//           ((event as React.KeyboardEvent).key === 'Tab' ||
-//             (event as React.KeyboardEvent).key === 'Shift')
-//         ) {
-//           return;
-//         }
-
-//         setState({ ...state, [anchor]: open });
-//       };
-
-//   const list = (anchor: Anchor) => (
-//     <Box
-//       sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 700 }}
-//       role="presentation"
-//       onClick={toggleDrawer(anchor, false)}
-//       onKeyDown={toggleDrawer(anchor, false)}
-//     >
-
-//     </Box>
-//   );
-
-//   return (
-//     <div>
-//       {(['left', 'right', 'top', 'bottom'] as const).map((anchor) => (
-//         <React.Fragment key={anchor}>
-//           <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-//           <SwipeableDrawer
-//             anchor={anchor}
-//             open={state[anchor]}
-//             onClose={toggleDrawer(anchor, false)}
-//             onOpen={toggleDrawer(anchor, true)}
-//           >
-//             {list(anchor)}
-//           </SwipeableDrawer>
-//         </React.Fragment>
-//       ))}
-//     </div>
-//   );
-// }
-
-
-// export default ITable
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React, { useEffect, useRef, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
@@ -183,7 +29,6 @@ const useStyles = makeStyles({
   drawer: {
     position: "relative",
     marginLeft: "auto",
-    width: 200,
     "& .MuiBackdrop-root": {
       display: "none"
     },
@@ -251,7 +96,7 @@ export default function BasicTable() {
         <Paper>
           <TableContainer>
             <Table className={classes.table} aria-label="simple table">
-              <TableHead>
+              <TableHead style={{marginRight: 200}}>
                 <TableRow>
                   <TableCell>Food (100g serving)</TableCell>
                   <TableCell align="right">Calories</TableCell>
